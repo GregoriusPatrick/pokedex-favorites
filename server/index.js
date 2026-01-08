37ba3f02f813b10ca6c7111df3e9e30275b5d7c6
GregoriusPatrick <patrickurniawan@gmail.com>
2026-01-08 22:04:35 +0700

    Enable SSL for Supabase Connection

--- server/index.js
+++ server/index.js
@@ -8,12 +8,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // === 1. KONFIGURASI DATABASE ===
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    // Tambahan WAJIB untuk Supabase/Vercel:
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 // === 2. MIDDLEWARE (Perantara) ===
